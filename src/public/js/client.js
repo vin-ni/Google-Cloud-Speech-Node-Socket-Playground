962cfb847183e5ae2f9c711b9671f3e6a509631f
Vinzenz Aubry <vinzenzaubry@gmail.com>
2018-10-18 15:03:52 +0200

    added new 16bit calculation

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -61,7 +61,8 @@ function initRecording() {
 
 function microphoneProcess(e) {
 	var left = e.inputBuffer.getChannelData(0);
-	var left16 = convertFloat32ToInt16(left);
+	// var left16 = convertFloat32ToInt16(left); // old 32 to 16 function
+	var left16 = downsampleBuffer(left, 44100, 16000)
 	socket.emit('binaryData', left16);
 }
 
@@ -272,4 +273,31 @@ function convertFloat32ToInt16(buffer) {
 		}
 	}
 	return buf.buffer
+}
+
+var downsampleBuffer = function (buffer, sampleRate, outSampleRate) {
+    if (outSampleRate == sampleRate) {
+        return buffer;
+    }
+    if (outSampleRate > sampleRate) {
+        throw "downsampling rate show be smaller than original sample rate";
+    }
+    var sampleRateRatio = sampleRate / outSampleRate;
+    var newLength = Math.round(buffer.length / sampleRateRatio);
+    var result = new Int16Array(newLength);
+    var offsetResult = 0;
+    var offsetBuffer = 0;
+    while (offsetResult < result.length) {
+        var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+        var accum = 0, count = 0;
+        for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+            accum += buffer[i];
+            count++;
+        }
+
+        result[offsetResult] = Math.min(1, accum / count)*0x7FFF;
+        offsetResult++;
+        offsetBuffer = nextOffsetBuffer;
+    }
+    return result.buffer;
 }
