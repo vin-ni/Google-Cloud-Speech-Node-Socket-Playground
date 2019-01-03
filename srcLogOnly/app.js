c1befde9f8e61b2ff8a628ac18cdb27105fbb0d8
vin-ni <vinzenz@weboffice.de>
2019-01-03 19:09:35 +0100

    added log only example

---
+++ srcLogOnly/app.js
@@ -0,0 +1,123 @@
+'use strict'
+
+//  Google Cloud Speech Playground with node.js and socket.io
+//  Created by Vinzenz Aubry for sansho 24.01.17
+//  Feel free to improve!
+//	Contact: vinzenz@sansho.studio
+
+const express = require('express'); // const bodyParser = require('body-parser'); // const path = require('path');
+const fs = require('fs');
+const environmentVars = require('dotenv').config();
+
+// Google Cloud
+const speech = require('@google-cloud/speech');
+const speechClient = new speech.SpeechClient(); // Creates a client
+
+
+const app = express();
+const port = process.env.PORT || 1337;
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server);
+
+app.use('/assets', express.static(__dirname + '/public'));
+app.use('/session/assets', express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+
+// =========================== ROUTERS ================================ //
+
+app.get('/', function (req, res) {
+    res.render('index', {});
+});
+
+app.use('/', function (req, res, next) {
+    next(); // console.log(`Request Url: ${req.url}`);
+});
+
+
+// =========================== SOCKET.IO ================================ //
+
+io.on('connection', function (client) {
+    console.log('Client Connected to server');
+    let recognizeStream = null;
+
+    client.on('join', function (data) {
+        client.emit('messages', 'Socket Connected to Server');
+    });
+
+    client.on('messages', function (data) {
+        client.emit('broad', data);
+    });
+
+    client.on('startGoogleCloudStream', function (data) {
+        startRecognitionStream(this, data);
+    });
+
+    client.on('endGoogleCloudStream', function (data) {
+        stopRecognitionStream();
+    });
+
+    client.on('binaryData', function (data) {
+        // console.log(data); //log binary data
+        if (recognizeStream !== null) {
+            recognizeStream.write(data);
+        }
+    });
+
+    function startRecognitionStream(client, data) {
+        recognizeStream = speechClient.streamingRecognize(request)
+            .on('error', console.error)
+            .on('data', (data) => {
+                process.stdout.write(
+                    (data.results[0] && data.results[0].alternatives[0])
+                        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+                        : `\n\nReached transcription time limit, press Ctrl+C\n`);
+                client.emit('speechData', data);
+
+                // if end of utterance, let's restart stream
+                // this is a small hack. After 65 seconds of silence, the stream will still throw an error for speech length limit
+                if (data.results[0] && data.results[0].isFinal) {
+                    stopRecognitionStream();
+                    startRecognitionStream(client);
+                    // console.log('restarted stream serverside');
+                }
+            });
+    }
+
+    function stopRecognitionStream() {
+        if (recognizeStream) {
+            recognizeStream.end();
+        }
+        recognizeStream = null;
+    }
+});
+
+
+// =========================== GOOGLE CLOUD SETTINGS ================================ //
+
+// The encoding of the audio file, e.g. 'LINEAR16'
+// The sample rate of the audio file in hertz, e.g. 16000
+// The BCP-47 language code to use, e.g. 'en-US'
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US'; //en-US
+
+const request = {
+    config: {
+        encoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: languageCode,
+        profanityFilter: false,
+        enableWordTimeOffsets: true
+    },
+    interimResults: true // If you want interim results, set this to true
+};
+
+
+// =========================== START SERVER ================================ //
+
+server.listen(port, "127.0.0.1", function () { //http listen, to make socket work
+    // app.address = "127.0.0.1";
+    console.log('Server started on port:' + port)
+});
---
+++ srcLogOnly/public/js/client.js
@@ -0,0 +1,199 @@
+'use strict'
+
+//connection to socket
+const socket = io.connect();
+
+//================= CONFIG =================
+// Stream Audio
+let bufferSize = 2048,
+	AudioContext,
+	context,
+	processor,
+	input,
+	globalStream;
+
+//vars
+let audioElement = document.querySelector('audio'),
+	finalWord = false,
+	resultText = document.getElementById('ResultText'),
+	removeLastSentence = true,
+	streamStreaming = false,
+	recognitionDataArray = [],
+	interimSentence = '',
+	finalSentence = '',
+	internCurrentHoCount = 0,
+	internTotalHoCount = 0,
+	publicTotalHoCount = 0;
+
+let params = {
+	startedRecording: false,
+}
+
+
+//audioStream constraints
+const constraints = {
+	audio: true,
+	video: false
+};
+
+//================= RECORDING =================
+
+
+
+function initRecording() {
+	socket.emit('startGoogleCloudStream', ''); //init socket Google Speech Connection
+	streamStreaming = true;
+	AudioContext = window.AudioContext || window.webkitAudioContext;
+	context = new AudioContext();
+	processor = context.createScriptProcessor(bufferSize, 1, 1);
+	processor.connect(context.destination);
+	context.resume();
+
+	var handleSuccess = function (stream) {
+		globalStream = stream;
+		input = context.createMediaStreamSource(stream);
+		input.connect(processor);
+
+		processor.onaudioprocess = function (e) {
+			microphoneProcess(e);
+		};
+	};
+
+	navigator.mediaDevices.getUserMedia(constraints)
+		.then(handleSuccess);
+
+}
+
+function microphoneProcess(e) {
+	var left = e.inputBuffer.getChannelData(0);
+	// var left16 = convertFloat32ToInt16(left); // old 32 to 16 function
+	var left16 = downsampleBuffer(left, 44100, 16000)
+	socket.emit('binaryData', left16);
+}
+
+
+
+
+//================= INTERFACE =================
+var startButton = document.getElementById("startRecButton");
+startButton.addEventListener("click", startRecording);
+
+var endButton = document.getElementById("stopRecButton");
+endButton.addEventListener("click", stopRecording);
+endButton.disabled = true;
+
+function startRecording() {
+	if (params.startedRecording){return}
+	params.startedRecording = true;
+	endButton.disabled = false;
+	initRecording();
+}
+
+function stopRecording() {
+	endButton.disabled = true;
+
+	if (!streamStreaming){return} // stop disconnecting if already disconnected;
+
+	streamStreaming = false;
+	socket.emit('endGoogleCloudStream', '');
+
+
+	let track = globalStream.getTracks()[0];
+	track.stop();
+
+	input.disconnect(processor);
+	processor.disconnect(context.destination);
+	context.close().then(function () {
+		input = null;
+		processor = null;
+		context = null;
+		AudioContext = null;
+		startButton.disabled = false;
+	});
+}
+
+//================= SOCKET IO =================
+socket.on('connect', function (data) {
+	socket.emit('join', 'Server Connected to Client');
+});
+
+
+socket.on('messages', function (data) {
+	console.log(data);
+});
+
+
+socket.on('speechData', function (data) {
+	// console.log(data.results[0].alternatives[0].transcript);
+	var dataFinal = undefined || data.results[0].isFinal;
+
+	if (dataFinal === false) {
+		let interimString = data.results[0].alternatives[0].transcript;
+		console.log(interimString);
+
+	} else if (dataFinal === true) {
+		//log final string
+		let finalString = data.results[0].alternatives[0].transcript;
+		console.log("Google Speech sent 'final' Sentence and it is:");
+		console.log(finalString);
+
+		finalWord = true;
+		endButton.disabled = false;
+		removeLastSentence = false;
+	}
+});
+
+//================= OTHER STUFF =================
+
+window.onbeforeunload = function () {
+	if (streamStreaming) { socket.emit('endGoogleCloudStream', ''); }
+};
+
+//================= SANTAS HELPERS =================
+
+// sampleRateHertz 16000 //saved sound is awefull
+function convertFloat32ToInt16(buffer) {
+	let l = buffer.length;
+	let buf = new Int16Array(l / 3);
+
+	while (l--) {
+		if (l % 3 == 0) {
+			buf[l / 3] = buffer[l] * 0xFFFF;
+		}
+	}
+	return buf.buffer
+}
+
+var downsampleBuffer = function (buffer, sampleRate, outSampleRate) {
+    if (outSampleRate == sampleRate) {
+        return buffer;
+    }
+    if (outSampleRate > sampleRate) {
+        throw "downsampling rate show be smaller than original sample rate";
+    }
+    var sampleRateRatio = sampleRate / outSampleRate;
+    var newLength = Math.round(buffer.length / sampleRateRatio);
+    var result = new Int16Array(newLength);
+    var offsetResult = 0;
+    var offsetBuffer = 0;
+    while (offsetResult < result.length) {
+        var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+        var accum = 0, count = 0;
+        for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+            accum += buffer[i];
+            count++;
+        }
+
+        result[offsetResult] = Math.min(1, accum / count)*0x7FFF;
+        offsetResult++;
+        offsetBuffer = nextOffsetBuffer;
+    }
+    return result.buffer;
+}
+
+function capitalize(s) {
+	if (s.length < 1) {
+		return s;
+	}
+	return s.charAt(0).toUpperCase() + s.slice(1);
+}
