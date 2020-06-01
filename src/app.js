ec6f7cbcb5a79096c07a23001543c57c30990ba5
Vinzenz Aubry <vinzenzaubry@gmail.com>
2020-06-01 22:51:14 +0200

    cleaned + npm updates

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 //  Google Cloud Speech Playground with node.js and socket.io
 //  Created by Vinzenz Aubry for sansho 24.01.17
@@ -6,13 +6,11 @@
 //	Contact: vinzenz@sansho.studio
 
 const express = require('express'); // const bodyParser = require('body-parser'); // const path = require('path');
-const fs = require('fs');
 const environmentVars = require('dotenv').config();
 
 // Google Cloud
 const speech = require('@google-cloud/speech');
 const speechClient = new speech.SpeechClient(); // Creates a client
-
 
 const app = express();
 const port = process.env.PORT || 1337;
@@ -24,75 +22,70 @@ app.use('/assets', express.static(__dirname + '/public'));
 app.use('/session/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-
 // =========================== ROUTERS ================================ //
 
 app.get('/', function (req, res) {
-    res.render('index', {});
+  res.render('index', {});
 });
 
 app.use('/', function (req, res, next) {
-    next(); // console.log(`Request Url: ${req.url}`);
+  next(); // console.log(`Request Url: ${req.url}`);
 });
-
 
 // =========================== SOCKET.IO ================================ //
 
 io.on('connection', function (client) {
-    console.log('Client Connected to server');
-    let recognizeStream = null;
+  console.log('Client Connected to server');
+  let recognizeStream = null;
 
-    client.on('join', function (data) {
-        client.emit('messages', 'Socket Connected to Server');
-    });
+  client.on('join', function () {
+    client.emit('messages', 'Socket Connected to Server');
+  });
 
-    client.on('messages', function (data) {
-        client.emit('broad', data);
-    });
+  client.on('messages', function (data) {
+    client.emit('broad', data);
+  });
 
-    client.on('startGoogleCloudStream', function (data) {
-        startRecognitionStream(this, data);
-    });
+  client.on('startGoogleCloudStream', function (data) {
+    startRecognitionStream(this, data);
+  });
 
-    client.on('endGoogleCloudStream', function (data) {
-        stopRecognitionStream();
-    });
+  client.on('endGoogleCloudStream', function () {
+    stopRecognitionStream();
+  });
 
-    client.on('binaryData', function (data) {
-        // console.log(data); //log binary data
-        if (recognizeStream !== null) {
-            recognizeStream.write(data);
-        }
-    });
-
-    function startRecognitionStream(client, data) {
-        recognizeStream = speechClient.streamingRecognize(request)
-            .on('error', console.error)
-            .on('data', (data) => {
-                process.stdout.write(
-                    (data.results[0] && data.results[0].alternatives[0])
-                        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-                        : `\n\nReached transcription time limit, press Ctrl+C\n`);
-                client.emit('speechData', data);
-
-                // if end of utterance, let's restart stream
-                // this is a small hack. After 65 seconds of silence, the stream will still throw an error for speech length limit
-                if (data.results[0] && data.results[0].isFinal) {
-                    stopRecognitionStream();
-                    startRecognitionStream(client);
-                    // console.log('restarted stream serverside');
-                }
-            });
+  client.on('binaryData', function (data) {
+    // console.log(data); //log binary data
+    if (recognizeStream !== null) {
+      recognizeStream.write(data);
     }
+  });
 
-    function stopRecognitionStream() {
-        if (recognizeStream) {
-            recognizeStream.end();
+  function startRecognitionStream(client) {
+    recognizeStream = speechClient
+      .streamingRecognize(request)
+      .on('error', console.error)
+      .on('data', (data) => {
+        process.stdout.write(data.results[0] && data.results[0].alternatives[0] ? `Transcription: ${data.results[0].alternatives[0].transcript}\n` : '\n\nReached transcription time limit, press Ctrl+C\n');
+        client.emit('speechData', data);
+
+        // if end of utterance, let's restart stream
+        // this is a small hack. After 65 seconds of silence, the stream will still throw an error for speech length limit
+        if (data.results[0] && data.results[0].isFinal) {
+          stopRecognitionStream();
+          startRecognitionStream(client);
+          // console.log('restarted stream serverside');
         }
-        recognizeStream = null;
+      });
+  }
+
+  function stopRecognitionStream() {
+    if (recognizeStream) {
+      recognizeStream.end();
     }
+    recognizeStream = null;
+  }
 });
-
 
 // =========================== GOOGLE CLOUD SETTINGS ================================ //
 
@@ -104,23 +97,23 @@ const sampleRateHertz = 16000;
 const languageCode = 'en-US'; //en-US
 
 const request = {
-    config: {
-        encoding: encoding,
-        sampleRateHertz: sampleRateHertz,
-        languageCode: languageCode,
-        profanityFilter: false,
-        enableWordTimeOffsets: true,
-        // speechContexts: [{
-        //     phrases: ["hoful","shwazil"]
-        //    }] // add your own speech context for better recognition
-    },
-    interimResults: true // If you want interim results, set this to true
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+    profanityFilter: false,
+    enableWordTimeOffsets: true,
+    // speechContexts: [{
+    //     phrases: ["hoful","shwazil"]
+    //    }] // add your own speech context for better recognition
+  },
+  interimResults: true, // If you want interim results, set this to true
 };
-
 
 // =========================== START SERVER ================================ //
 
-server.listen(port, "127.0.0.1", function () { //http listen, to make socket work
-    // app.address = "127.0.0.1";
-    console.log('Server started on port:' + port)
+server.listen(port, '127.0.0.1', function () {
+  //http listen, to make socket work
+  // app.address = "127.0.0.1";
+  console.log('Server started on port:' + port);
 });
